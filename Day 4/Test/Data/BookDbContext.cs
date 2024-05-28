a81aac7962c4668b1003243948d4ff89e82eee6c
RuchitUkani <155770788+RuchitUkani@users.noreply.github.com>
2024-05-28 12:30:19 +0530

    Daily task 4

---
+++ Day 4/Test/Data/BookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Models;
+
+namespace Test.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.Role> Roles { get; set; }
+        public DbSet<Models.Book> Books { get; set; }
+
+    }
+}
---
+++ Day 4/Test/Service/BookService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Test.Data;
+using Test.Models;
+
+namespace Test.Service
+{
+    public class BookService
+    {
+        private readonly BookDbContext _context;
+
+        public BookService(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Book> GetAll() => _context.Books.ToList();
+
+        public Book GetById(int id) => _context.Books.FirstOrDefault(b => b.Id == id);
+
+        public void Add(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public void Update(Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
