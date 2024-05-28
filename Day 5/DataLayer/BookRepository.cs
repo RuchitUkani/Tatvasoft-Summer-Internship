5bc94157af3408bd54a0f6df22fc0631db7d4701
RuchitUkani <155770788+RuchitUkani@users.noreply.github.com>
2024-05-28 12:28:40 +0530

    Daily task 5

---
+++ Day 5/DataLayer/BookRepository.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Context;
+using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class BookRepository
+    {
+        private AppDbContext _appDbContext;
+
+        public BookRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task InsertBook(BookDetails bookDetails)
+        {
+            await _appDbContext.AddAsync(bookDetails);
+            await _appDbContext.SaveChangesAsync();
+        }
+  
+    }
+}
---
+++ Day 5/ServiceLayer/BookService.cs
@@ -0,0 +1,45 @@
+﻿using DataLayer;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.Model;
+
+namespace ServiceLayer
+{
+    public class BookService
+    {
+        private readonly List<Book> _book;
+        private readonly BookRepository _bookRepository;
+
+        public BookService(BookRepository bookRepository)
+        {
+            _book = new List<Book>() {
+              new Book() { Id = 1 , Title = "Book1" , Author = "ruchit" , Genre ="first"},
+              new Book() { Id = 2 , Title = "Book2" , Author = "vikas" , Genre ="first"}
+            };
+            _bookRepository = bookRepository;
+        }   
+
+        public List<Book> GetAll()
+        {
+            return _book;
+        }
+
+        public Book GetById(int id)
+        {
+            return _book.First(x => x.Id == id);
+        }
+
+        public List<Book> InsertBook(Book book)
+        {
+            _book.Add(book);
+            return _book;
+        }
+
+        public async Task InsertBookDetails(BookDetails bookDetails)
+        {
+            await _bookRepository.InsertBook(bookDetails);
+        }
+
+    
+    }
+}
